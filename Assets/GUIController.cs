f617f35b82d65e5cf793d5221dd0076cade6759b
Yamist <8bityamist@gmail.com>
2020-04-09 14:23:27 -0700

    first commit

---
+++ Assets/GUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class GUIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+        string connection = "URI=file:" + Application.persistentDataPath + "/" + "My_Database";
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        IDbCommand dbcmd;
+        IDataReader reader;
+
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable =
+          "CREATE TABLE IF NOT EXISTS " + "my_table" + " (" +
+          "id" + " INTEGER PRIMARY KEY, " +
+          "val" + " INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        reader = dbcmd.ExecuteReader();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Virtual Advisor
+
+Project designed for CS 360 that personalizes your college class schedule.
+
+Project is dependent upon sqlite-unity-plugin: https://github.com/rizasif/sqlite-unity-plugin
+
+## Getting Started
+
+TBA
+
+### Prerequisites
+
+Unity 2019.3.2f1
+
+### Installing
+
+Install via Unity Hub
+
+## Running
+
+TBA
